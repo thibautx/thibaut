1d4375924fa3db33d32f8a0cfc188e79e66a7fbd
Thibaut Xiong <txiong3@illinois.edu>
2016-11-22 11:38:03 -0600

    procfile

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object('config')
 
-app.route('/')
+@app.route('/')
 def home():
     return 'foobar'
     # return render_template('index.html')
