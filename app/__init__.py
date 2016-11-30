9db2dfed56a731d4492460dc2f2bbdef36680663
Thibaut Xiong <txiong3@illinois.edu>
2016-11-30 15:06:35 -0600

    triplebyte

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask
+# from flask_admin import Admin
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+migrate = Migrate(app, db)
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app.about import about_module as mod_about
-from app.projects import projects_module as mod_projects
+# Register Blueprints
+from app.about.controllers import about_module as mod_about
+from app.projects.controllers import projects_module as mod_projects
+from app.triplebyte.controllers import triplebyte_module as mod_triplebyte
 app.register_blueprint(mod_about)
 app.register_blueprint(mod_projects)
-
+app.register_blueprint(mod_triplebyte)
---
+++ app/triplebyte/controllers.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, render_template
+
+triplebyte_module = Blueprint('tiplebyte', __name__, url_prefix='/triplebyte')
