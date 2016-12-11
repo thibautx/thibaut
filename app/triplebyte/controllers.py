2556d8856e97944adc6c1389bf1b417304fa4ab8
Thibaut Xiong <txiong3@illinois.edu>
2016-12-11 15:41:16 -0600

    triplebyte

--- app/triplebyte/controllers.py
+++ app/triplebyte/controllers.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template
 
 triplebyte_module = Blueprint('tiplebyte', __name__, url_prefix='/triplebyte')
+
+@triplebyte_module.route('/')
+def triplebyte():
+    return render_template('triplebyte/index.html')
